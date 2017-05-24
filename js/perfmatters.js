ed8c8b937b6f21422a4f321a3c63cd0af8eeb914
Pablo Muñoz <pablodavid.munoz@gmail.com>
2017-05-24 17:48:42 +0200

    Final Project - Undo customization

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -8,6 +8,13 @@ WebFontConfig = {
 
 };
 
+(function(d) {
+    var wf = d.createElement('script'), s = d.scripts[0];
+    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+    wf.async = true;
+    s.parentNode.insertBefore(wf, s);
+})(document);
+
 function logCRP() {
   var t = window.performance.timing,
     dcl = t.domContentLoadedEventStart - t.domLoading,
@@ -18,29 +25,4 @@ function logCRP() {
 
 window.addEventListener("load", function(event) {
   logCRP();
- 
-  (function(d) {
-    var wf = d.createElement('script'), s = d.scripts[0];
-    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
-    wf.async = true;
-    s.parentNode.insertBefore(wf, s);
-})(document);
-});
-
-window.addEventListener("load", function(event) {
-
-  logCRP();
-
-  var container = document.querySelector(".container").getAttribute('id');
-  onFirstLoad[container]();
-
-  var header = document.querySelector("h1");
-  header.addEventListener("click", onPressHeader);
-
-  var navLinks = document.querySelectorAll("nav .nav-item");
-  for (var i = 0; i < navLinks.length; i++) {
-    navLinks[i].addEventListener('click', onNavClick);
-  }
-
-
 });
